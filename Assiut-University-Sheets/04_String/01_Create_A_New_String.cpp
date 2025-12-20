3d61407c3c437bd33ee53e0b31cea67aa785944c
Mohamed Waleed <m7dwaleed@gmail.com>
2025-12-20 21:00:38 +0200

    udate solution

--- Assiut-University-Sheets/04_String/01_Create_A_New_String.cpp
+++ Assiut-University-Sheets/04_String/01_Create_A_New_String.cpp
@@ -2,14 +2,28 @@
 #include <string>
 using namespace std;
 
+
+/*
+? what is reqired 
+? here are two string print length of each and concatinate them
+?
+?the solution
+?you use string_name.length() to calculate the size of each string
+?and use + to concatinate two strings or use operator << and print each string
+?
+?
+
+
+*/
 int main()
 {
     string s, m;
-    cin >> s >> m;
-    int Length_S = s.length();
-    int Lenght_M = m.length();
+    getline(cin,s);
+    getline(cin,m);
+    size_t length_s = s.length();
+    size_t length_m = m.length();
 
-    cout<<Length_S<<" "<<Lenght_M<<endl;
-    string Con=s+" "+m;
-    cout<<Con<<endl;
+    cout<<length_s<<" "<<length_m<<endl;
+    cout<<s<<" "<<m;
+    
 }
