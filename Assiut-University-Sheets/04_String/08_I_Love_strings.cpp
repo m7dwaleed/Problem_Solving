556437edf7addfe35cd97282d7a28d35df5943bd
Mohamed Waleed <m7dwaleed@gmail.com>
2025-12-21 12:48:36 +0200

    problem : CF 219856 K

--- Assiut-University-Sheets/04_String/08_I_Love_strings.cpp
+++ Assiut-University-Sheets/04_String/08_I_Love_strings.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 using namespace std;
 
 int main()
 {
+ios::sync_with_stdio(0);
+cin.tie(0);
+
 
     int n;
     cin >> n;
-    int sz3;
-    string s1, s2, s3;
-    for (int i = 0; i < n; i++)
+
+    while (n--)
     {
 
+        string s1, s2;
         cin >> s1 >> s2;
 
         size_t sz1 = s1.length();
         size_t sz2 = s2.length();
-
-        sz3=max(sz1,sz2);
-          
-        for (int k = 0; k < sz3; k++)
+        int sz3 = max(sz1, sz2);
+        for (size_t i = 0; i < sz3; i++)
         {
-           int i=0;
-          int j=0;
-           if(i<sz1){
-            cout<<s1[i];
-            i++;
-           }
-           if(j<sz2){
-            cout<<s2[j];
-            j++;
-           }
+            if (i < sz1)
+            {
+                cout << s1[i];
+            }
+            if (i < sz2)
+            {
+                cout << s2[i];
+            }
         }
-        cout<<endl;
-    }
 
-   
+        cout << "\n";
+    }
 
     return 0;
 }
